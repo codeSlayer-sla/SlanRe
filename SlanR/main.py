ecc3c9f9561382f9806fc178d919549c3ad1829f
codeSlayer-sla <142634716+codeSlayer-sla@users.noreply.github.com>
2023-08-25 18:46:48 -0500

    Add files via upload

---
+++ SlanR/main.py
@@ -0,0 +1,169 @@
+import redis
+from tabulate import tabulate
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)
+
+
+
+class Slang:
+    words_generated = False
+
+    def __init__(self, word, meaning):
+        self.word = word
+        self.meaning = meaning
+
+
+
+def sys_msg(types, msg):
+    print(f"\n-----> {types}: {msg} <------")
+
+
+
+def exists(value):
+    if r.exists(value) != 1:
+        return False
+    else:
+        return True
+
+
+
+def input_word():
+    return input("Ingrese la palabra >> ")
+
+
+
+def add_word():
+    word = input_word()
+    if not exists(f'{word}'):
+        meaning = input("Ingrese el significado >> ")
+        new_slang = Slang(word, meaning)
+        r.set(f'{new_slang.word}', f'{new_slang.meaning}')
+        sys_msg("INFO", "Palabra agregada")
+    else:
+        sys_msg("ERROR", "La palabra ya existe")
+
+
+
+def edit_word():
+    word = input_word()
+    if not exists(f'{word}'):
+        sys_msg("ERROR", "La palabra no existe")
+    else:
+        meaning = input("Ingrese el nuevo significado >> ")
+        r.set(word, f'{meaning}')
+        sys_msg("INFO", "Palabra editada")
+
+
+
+def del_word():
+    word = input("Ingrese la palabra >> ")
+    if not exists(f'{word}'):
+        sys_msg("ERROR", "La palabra no existe")
+    else:
+        r.delete(word)
+        sys_msg("INFO", "La palabra ha sido eliminada")
+
+
+
+def get_words():
+    values = r.keys("*")
+    table = []
+    for i in values:
+        table.append([i, r.get(i)])
+    print(tabulate(table, headers=["Palabra", "Significado"], tablefmt="psql"))
+
+
+
+def get_meaning():
+    word = input_word()
+    if not exists(word):
+        sys_msg("ERROR", "La palabra no existe")
+    else:
+        print(f"\n{word}, significa: {r.get(word)}")
+
+
+LY)
+def generate_data():
+    if Slang.words_generated:
+        sys_msg("ERROR", "Los registros ya han sido creados")
+    else:
+        slang_objects = [
+            Slang("Chombo", "Amigo cercano o compañero"),
+            Slang("Jato", "Casa o hogar"),
+            Slang("Pana", "Amigo o camarada"),
+            Slang("Que xopa!", "El clasico saludo de nosotros"),
+            Slang("Taquilla", "Alguna historia o relato que puede ser falsa"),
+            Slang("Quilla", "Dinero"),
+            Slang("Tirar la posta", "Contar una historia o chisme"),
+            Slang("Taquear", "Comer en exceso"),
+            Slang("Chiri", "Frío"),
+            Slang("Pelea de gallos", "Competencia o disputa acalorada")
+        ]
+        for i in slang_objects:
+            r.set(i.word, i.meaning)
+        sys_msg("INFO", "Se han creado 10 registros")
+        Slang.words_generated = True
+
+
+
+def delete_all_data():
+    sys_msg('WARNING', 'Seguro que desea eliminar todos los registros?')
+    if input("\ttype 'y' to confirm >> ") == 'y':
+        r.flushdb()
+        sys_msg("INFO", "Todos los registros han sido eliminados")
+    else:
+        sys_msg("INFO", "Eliminacion cancelada")
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        
+        print("\nBIENVENIDO AL DICCIONARIO DE SLANG PANAMEÑO\n")
+
+        
+        menu = """
+                1) Agregar nueva palabra
+                2) Editar palabra
+                3) Eliminar palabra 
+                4) Ver palabras
+                5) Buscar significado de palabra
+                6) Insertar 10 registros(only test usage)
+                7) Eliminar todos los registros(only test usage)
+                8) Salir
+                """
+
+        
+        end = False
+
+        while not end:
+            try:
+                print(menu)
+                option = int(input(f"Ingresa una opcion: "))
+                if int(option) in range(1, 9):
+                    match option:
+                        case 1:
+                            add_word()
+                        case 2:
+                            edit_word()
+                        case 3:
+                            del_word()
+                        case 4:
+                            get_words()
+                        case 5:
+                            get_meaning()
+                        case 6:
+                            generate_data()
+                        case 7:
+                            delete_all_data()
+                        case 8:
+                            sys_msg("INFO", "El programa ha finalizado")
+                            exit()
+                else:
+                    sys_msg("ERROR", "Ingrese una opcion correcta")
+            except ValueError:
+                sys_msg("ERROR", "Ingrese una opcion correcta")
+    except (KeyboardInterrupt, EOFError):
+        sys_msg("INFO", "El programa ha finalizado")
